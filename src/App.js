0229c6189106036f4db28d56025e06bebe7f247a
munji <bmj3222@naver.com>
2023-09-29 12:26:37 +0900

    코딩애플 object state 변경 과제

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import {useState} from 'react';
 
 function App() {
 
-  let post = '매캐매캐~';
-  let [글제목1, b] = useState('남자 코트 추천');
-  let [글제목2, c] = useState('강남 우동맛집');
-  let [글제목3, d] = useState('파이썬독학');
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [좋아요, 좋아요변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>먼지</h4>
+        <h4>Blog</h4>
       </div>
+      <button onClick={()=>{
+        글제목변경(['여자코트 추천', '강남 우동맛집', '파이썬독학']);
+      }}>글수정</button>
+      
       <div className="list">
-          <h4>{ 글제목1 }</h4>
+          <h4>{ 글제목[0] }<span onClick={()=>{좋아요변경(좋아요+1)}}>👍 </span>{좋아요}</h4>
           <p>9월 20일 발행</p>
       </div> 
       <div className="list">
-          <h4>{ 글제목2 }</h4>
+          <h4>{ 글제목[1] }</h4>
           <p>9월 20일 발행</p>
       </div> 
       <div className="list">
-          <h4>{ 글제목3 }</h4>
+          <h4>{ 글제목[2] }</h4>
           <p>9월 20일 발행</p>
       </div> 
     </div>
