f7b8ae6eb02327afd8195971e5913c9d534fcb6c
munji <bmj3222@naver.com>
2023-09-29 12:26:37 +0900

    코딩애플 object state 변경 과제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
 
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
@@ -46,7 +28,6 @@ function App() {
           <h4>{ 글제목[2] }</h4>
           <p>9월 20일 발행</p>
       </div> 
->>>>>>> 640c5f2
     </div>
   );
 }
