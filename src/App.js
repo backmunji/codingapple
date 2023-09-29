6915466ee0a394ba110187e18baf22412d84a9e2
munji <bmj3222@naver.com>
2023-09-29 12:26:37 +0900

    코딩애플 object state 변경 과제

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
 
 function App() {
-
-  let post = '매캐매캐~';
-  let [글제목1, b] = useState('남자 코트 추천');
-  let [글제목2, c] = useState('강남 우동맛집');
-  let [글제목3, d] = useState('파이썬독학');
-
   return (
     <div className="App">
-      <div className="black-nav">
-        <h4>먼지</h4>
-      </div>
-      <div className="list">
-          <h4>{ 글제목1 }</h4>
-          <p>9월 20일 발행</p>
-      </div> 
-      <div className="list">
-          <h4>{ 글제목2 }</h4>
-          <p>9월 20일 발행</p>
-      </div> 
-      <div className="list">
-          <h4>{ 글제목3 }</h4>
-          <p>9월 20일 발행</p>
-      </div> 
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
